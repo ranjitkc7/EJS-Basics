eaaa2695a085b19936a2d23da2a5919e2be2d468
ranjitkc7 <ranjit252@gmail.com>
2025-06-05 22:43:42 +0545

    second-commit

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+const filesDir = path.join(__dirname, "files");
+if (!fs.existsSync(filesDir)) {
+  fs.mkdirSync(filesDir);
+}
+
 app.get("/", (req, res) => {
-    res.render("index");
+  fs.readdir(filesDir, (err, files) => {
+    if (err) {
+      console.error("Error reading files directory:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+    res.render("index", { files });
+  });
+});
+app.post("/create", (req, res) => {
+  const fileName = req.body.title.split(" ").join("") + ".txt";
+  const filePath = path.join(filesDir, fileName);
+  fs.writeFile(filePath, req.body.details, (err) => {
+    if (err) {
+      console.error("File writing error:", err);
+      return res.status(500).send("Error saving file");
+    }
+    res.redirect("/");
+  });
 });
 
-//Dynamic Routing
-app.get("/profile/:username/:age", (req, res) => {
-    res.send(`Hello ${req.params.username} your age is ${req.params.age}`);
-})
-app.listen(3000,() => {
-    console.log("Server is running on port 3000");
-})
-// console.log(__dirname, "/public");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- public/javaScripts/script.js
+++ public/javaScripts/script.js
@@ -1,1 +1,1 @@
-alert("Hello Ranjit");
+// alert("Hello Ranjit");
